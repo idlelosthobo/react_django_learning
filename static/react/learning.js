633e5cb8e8dba1540c058471ff40aacf3e6cc7b1
Nathan <drugen@gmail.com>
2020-10-31 23:57:38 -0600

    update

--- static/react/learning.js
+++ static/react/learning.js
@@ -1,25 +1,25 @@
 class WelcomeBack extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            name: 'Jay',
-            appVersion: '3'
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Jay',
+      appVersion: '3'
     }
+  }
 
-    render(){
-        return(
-            <>
-                <h2>Hello {this.state.name || 'Friend'}! Welcome Back.</h2>
-                {
-                    this.state.appVersion && this.state.appVersion < 2
-                    ? <p>Your app is out of date. Please download the new version and take a look at all the new features.</p> 
-                    : ''
-                }
-                <CoolButton customText={this.state.appVersion && this.state.appVersion < 2 ? 'Download v2' : 'Download'} />
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h2>Hello {this.state.name || 'Friend'}! Welcome Back.</h2>
+        {
+          this.state.appVersion && this.state.appVersion < 2
+            ? <p>Your app is out of date. Please download the new version and take a look at all the new features.</p>
+            : ''
+        }
+        <CoolButton customText={this.state.appVersion && this.state.appVersion < 2 ? 'Download v2' : 'Download'}/>
+      </>
+    )
+  }
 
 
 }
